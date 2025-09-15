6c6e6d995400c4b7a96ad43a0cc948dc9da40f2c
Serra Kangas <kangasgames789@gmail.com>
2025-09-14 22:50:38 -0500

    lsp initial stuff

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,7 @@
 return {
-	"neovim/nvim-lspconfig"
+	"neovim/nvim-lspconfig",
+	config = function()
+		local servers = { "html", "cssls", "jdtls", "rust_analyzer", "pylsp", "pyrefly", "clangd", "yaml_language_server", "nil_ls", "lua_ls"  }
+		vim.lsp.enable(servers)
+	end,
 }
