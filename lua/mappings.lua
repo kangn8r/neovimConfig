01b3df0f27ef8b54679ea3b5d155b7bd1ac8d2b3
Serra Kangas <kangasgames789@gmail.com>
2025-09-14 21:31:56 -0500

    add which-key

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,6 +9,9 @@ map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
 -- Telescope
 local builtin = require('telescope.builtin')
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
@@ -23,3 +26,6 @@ map("n", "<C-n>", ":Neotree toggle<CR>")
 -- BarBar
 map('n', '<S-tab>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<tab>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<Leader><tab>',   '<Cmd>BufferPick<CR>', opts)
+map('n', '<Leader>x', '<Cmd>BufferClose<CR>', opts)
+map('n', '<Leader>X', '<Cmd>BufferCloseAllButCurrent<CR>', opts)
--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -7,9 +7,9 @@ return {
     init = function() vim.g.barbar_auto_setup = false end,
     opts = {
 			auto_hide = 1,
-			sidebar_filetypes = {
-				['neo-tree'] = {event = 'BufWipeout'},
-			},
+			-- sidebar_filetypes = {
+			-- 	['neo-tree'] = {event = 'BufWipeout'},
+			-- },
 			animation = false,
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
